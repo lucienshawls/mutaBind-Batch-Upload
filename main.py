70261fd0d295c453c520b0fff3b19793795149fb
Lucien Shaw <myxlc55@outlook.com>
2022-11-09 18:37:25 +0800

    feat: 单次任务提交

---
+++ main.py
@@ -0,0 +1,106 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import csv
+import os
+
+def myclick(driver,by,ele): #Click on the ele(ment) by ActionChains
+    spot=driver.find_element(by=by,value=ele)
+    ActionChains(driver).click(spot).perform()
+
+def myenter(driver,by,ele,word): #Put the word in the ele(ment)
+    spot=driver.find_element(by=by,value=ele)
+    spot.send_keys(Keys.CONTROL,'a') #Select all, making sure it's empty
+    spot.send_keys(word)
+
+def generate_mutations_file(mydata):
+    data_set = []
+    prefix = []
+    if not mydata['is_single']:
+        prefix = ['M1']
+    for m in mydata['mutations']:
+        data = prefix + [m[1],m[0]+m[2:-1],m[-1]]
+        data_set.append(data)
+    # print(data_set)
+    with open('temp.tsv','w',encoding='utf-8',newline='') as f:
+        writer = csv.writer(f,delimiter='\t')
+        writer.writerows(data_set)
+    return os.path.abspath('temp.tsv')
+
+
+def init():
+    # myoption = [
+    #     '--headless',
+    #     'window-size=1920x1080'
+    # ]
+    myoption = []
+    option = webdriver.EdgeOptions()
+    for i in myoption:
+        option.add_argument(i)
+    option.add_experimental_option('detach',True) # 程序结束后保留浏览器窗口
+    option.add_experimental_option('excludeSwitches',['enable-logging']) # 关闭selenium控制台提示
+    driver = webdriver.Edge(options=option)
+    driver.implicitly_wait(20)
+    driver.maximize_window() # 最大化
+    return driver
+
+def upload(driver,mydata):
+    driver.get('https://lilab.jysw.suda.edu.cn/research/mutabind2/research/mutabind2/')
+    myenter(driver,By.ID,'pdb_id_input',mydata['pdb'])
+    if mydata['is_single']:
+        myclick(driver,By.ID,'single')
+    else:
+        myclick(driver,By.ID,'multiple')
+    
+    index = 1
+    p1 = 0
+    p2 = 0
+    while True:
+        root_xpath = '//*[@id="gallery"]/li[%d]' %(index)
+        '//*[@id="gallery"]/li[1]/h5'
+        li_tag = driver.find_element(by=By.XPATH,value='%s/h5' %(root_xpath))
+        if li_tag.text in mydata['partner1']:
+            myclick(driver,By.XPATH,'%s/a[1]' %(root_xpath))
+            p1 += 1
+            time.sleep(0.5)
+        elif li_tag.text in mydata['partner2']:
+            myclick(driver,By.XPATH,'%s/a[2]' %(root_xpath))
+            p2 += 1
+            time.sleep(0.5)
+        else:
+            index += 1
+        
+        if p1 >= len(mydata['partner1']) and p2 >= len(mydata['partner2']):
+            break
+    myclick(driver,By.ID,'submit_partners')
+    
+    file_name = generate_mutations_file(mydata)
+    if mydata['is_single']:
+        myclick(driver,By.XPATH,'//*[@id="myTab"]/li[2]/a')
+        time.sleep(0.5)
+        driver.find_element(By.XPATH,'//*[@id="uploadFile"]/div/form/div/div[2]/input').send_keys(file_name)
+        myclick(driver,By.ID,'identify_submit_button')
+    else:
+        myclick(driver,By.ID,'optionsRadios2')
+        time.sleep(0.5)
+        driver.find_element(By.XPATH,'/html/body/div[2]/div[2]/form/div[1]/div[3]/input').send_keys(file_name)
+        myclick(driver,By.ID,'submit_partners')
+    return driver.current_url
+
+def main():
+    mydata = {
+        'pdb': '1A22',
+        'partner1': 'A',
+        'partner2': 'B',
+        'is_single': True,
+        'mutations': ['YA164A']
+    }
+    driver = init()
+    url = upload(driver=driver,mydata=mydata)
+    print(url)
+
+if __name__ == '__main__':
+    main()
